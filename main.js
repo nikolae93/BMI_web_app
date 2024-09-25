7c473a72dd62b30fd33e3965c71053f99e291020
Nikola Ekmedzic <nikola.ekmedzic@direkcija.rzzo.sr.gov.yu>
2024-09-25 08:10:44 +0200

    Implement resetInputs function

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // console.log("JS connected");
 
+document.getElementById("reset_inputs").addEventListener("click",resetInputs);
 
+
+function resetInputs(){
+    document.getElementById("height").value = "";
+    document.getElementById("weight").value = "";
+}
